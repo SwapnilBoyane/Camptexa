df6624dfe92101639e4bab1df6b866794fd915cb
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-05-08 14:47:18 -0500

    Create 01a_depth.sh

---
+++ 01a_depth.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=genotype
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=4
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-19
+
+#load modules
+conda activate samtools
+module load gcc/10.1.0
+module load r/4.3.0
+R
+
+# Set working directory
+workdir=/lustre/scratch/sboyane/camptexa
+
+basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail -n1 )
+
+# Calculate the depth of coverage per site
+~/anaconda3/envs/samtools/bin/samtools depth -a ${workdir}/01_bam_files/${basename_array}_final.bam > ${workdir}/depth/${basename_array}_depth.txt
+
+# Isolate the third column with the read counts only to reduce file size
+cut -f3 ${workdir}/depth/${basename_array}_depth.txt > ${workdir}/depth/${basename_array}_depth_reduced.txt
+
+# Remove the full depth file
+rm ${workdir}/depth/${basename_array}_depth.txt
+
+# Make sure to keep the ".R" script in same folder of this "".sh" script
+
+Rscript plot_coverage.R
