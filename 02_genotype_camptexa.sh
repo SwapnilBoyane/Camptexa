74984b6c4c077c3dbc19d28717191b37f4bdbc19
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2025-05-08 14:05:38 -0500

    Create 02_genotype_camptexa.sh

---
+++ 02_genotype_camptexa.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=genotype
+#SBATCH --partition=nocona
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=16G
+#SBATCH --array=1-19
+
+# Prepend bcftools path to PATH
+conda activate bcftools
+conda activate vcftools
+
+# define main working directory
+workdir=/lustre/scratch/sboyane/camptexa
+
+basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail -n1 )
+
+# define the reference genome
+refgenome=/lustre/work/sboyane/ref/camp_sp_genome_filtered.fasta
+
+# run bcftools to genotype
+bcftools mpileup --skip-indels -C 0 -d 200 --min-MQ 10 --threads 4 -f ${refgenome} ${workdir}/01_bam_files/${basename_array}_final.bam | bcftools call -m --threads 4 -o ${workdir}/02_vcf/${basename_array}.vcf
+
+# bgzip
+~/anaconda3/bin/bgzip ${workdir}/02_vcf/${basename_array}.vcf
+
+#tabix
+~/anaconda3/bin/tabix ${workdir}/02_vcf/${basename_array}.vcf.gz
+
+# filter individual vcf files
+bcftools view -i 'MIN(DP)>5' ${workdir}/02_vcf/${basename_array}.vcf.gz > ${workdir}/03_vcf/${basename_array}.vcf
+
+# bgzip
+~/anaconda3/bin/bgzip ${workdir}/03_vcf/${basename_array}.vcf
+
+#tabix
+~/anaconda3/bin/tabix ${workdir}/03_vcf/${basename_array}.vcf.gz
