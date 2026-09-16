13ba5697c70aafc668ca698385c8c876c0729883
Swapnil <135766321+SwapnilBoyane@users.noreply.github.com>
2026-09-16 17:24:24 -0500

    Update 01_make_gadma_input.sh

--- 05_demographic_analysis/01_make_gadma_input.sh
+++ 05_demographic_analysis/01_make_gadma_input.sh
@@ -16,5 +16,5 @@ source activate easySFS
 ~/easySFS/easySFS.py \
   -i camptexa_gadma_biallelic.vcf \
   -p popmap_gadma.txt \
-  -o easysfs_out_new \
+  -o easysfs_outgroup_new \
   -a --proj=4, 10, 20 --total-length 196670629
